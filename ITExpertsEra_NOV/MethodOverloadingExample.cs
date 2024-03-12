c484d6bc87c4728f0ee19c4413eee2be0e14020d
Tanushree Agarwal <138988881+TANUSHRIAGARWAL@users.noreply.github.com>
2024-03-12 15:03:12 +0530

    create new class

---
+++ ITExpertsEra_NOV/MethodOverloadingExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITExpertsEra_NOV
+{
+    public class MethodOverloadingExample
+    {
+   
+            public void Sum()
+            {
+                Console.WriteLine("I am default method");
+            }
+
+            public void Sum(int a)
+            {
+                Console.WriteLine("I am a method with parameter " + a);
+            }
+
+            public void Sum(string a)
+            {
+                Console.WriteLine("I am a method with parameter " + a);
+            }
+
+            public void Sum(double a, double b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+        }
+    }
+}
+}
